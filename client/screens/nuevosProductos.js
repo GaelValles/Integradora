e25a7825002f2a21598ccbb7a00fa8c8571bf1a8
Urielcont <Urielalvaro3@gmail.com>
2024-02-28 12:34:11 -0600

    ajuste nuevosProductos

--- client/screens/nuevosProductos.js
+++ client/screens/nuevosProductos.js
@@ -202,7 +202,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 10,
-    marginTop: 110,
+    marginTop: 30,
   },
   title: {
     fontSize: 30,
