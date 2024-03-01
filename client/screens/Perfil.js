30ec2ae883feca06f84a4c48da6c0ecd6a8110f1
FERBONILLA-wq <84981941+FERBONILLA-wq@users.noreply.github.com>
2024-03-01 10:22:59 -0600

    Delete client/screens/Perfil.js

--- client/screens/Perfil.js
+++
@@ -1,9 +0,0 @@
-import {View, Text} from "react-native"
-
-export default Perfil=() => {
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: 'bold', color: '#383838'}}>Esta es la de Perfil</Text>
-        </View>
-    )
-}
