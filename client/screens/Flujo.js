04fe36626f00d2c24102f7881df5847ad2f3f7e3
FERBONILLA-wq <84981941+FERBONILLA-wq@users.noreply.github.com>
2024-03-01 10:22:13 -0600

    Delete client/screens/Flujo.js

--- client/screens/Flujo.js
+++
@@ -1,9 +0,0 @@
-import {View, Text} from "react-native"
-
-export default Flujo= () => {
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: 'bold', color: '#383838'}}>Esta es la de Flujo</Text>
-        </View>
-    )
-}
