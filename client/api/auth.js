45e90883c45121326ca18eee73c0f0bcf1bdbe47
Medal05 <roberto.sierram17@gmail.com>
2024-04-08 00:41:22 -0600

    graficas

--- client/api/auth.js
+++ client/api/auth.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 // 192.168.1.14
 // 172.20.98.155
+// 192.168.1.71 (rob)
 // Cambiar la ip segun la red local que utilices
-const api = "http://192.168.1.14:3000/api";
+const api = "http://192.168.1.71:3000/api";
 
 export const regis = async (User) => axios.post(`${api}/registrarse`, User);
 
